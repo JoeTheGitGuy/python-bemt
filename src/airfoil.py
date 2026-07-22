d67c3b56cd01daccd47c1d7b61dafc8760631a33
JoeTheGitGuy <268192138+JoeTheGitGuy@users.noreply.github.com>
2026-07-22 23:53:23 +0100

    Added CL interpolation method to aerofoil class

--- src/airfoil.py
+++ src/airfoil.py
@@ -3,13 +3,37 @@ import csv
 class Aerofoil:
     def __init__(self,name,alpha,cl,cd,cdp,cm):
         self.name = name;
-        self.alpha = alpha;
-        self.cl = cl;
-        self.cd = cd;
-        self.cdp = cdp;
-        self.cm = cm;
-        
+        self.alpha_polar = alpha;
+        self.cl_polar = cl;
+        self.cd_polar = cd;
+        self.cdp_polar = cdp;
+        self.cm_polar = cm;
 
+    # calculate CL via interpolation for given AoA
+    def cl(self,alpha):
+        # check alpha in polar range
+        if alpha < min(self.alpha_polar) or alpha > max(self.alpha_polar):
+            return 0
+
+        # find interpolation bounds
+        n = len(self.alpha_polar)-1
+        for i in range(n):
+            if abs(self.alpha_polar[i] - alpha) < 0.00001:
+                return self.cl_polar[i]
+            elif (self.alpha_polar[i] > alpha):
+                lb_ind = i-1;
+                ub_ind = i;
+                break
+
+        # interpolate CL
+        dalpha  = self.alpha_polar[ub_ind]-self.alpha_polar[lb_ind];
+        dcl     = self.cl_polar[ub_ind]-self.cl_polar[lb_ind];
+
+        return self.cl_polar[lb_ind] + (alpha-self.alpha_polar[lb_ind])*(dcl/dalpha);
+        
+        
+        
+# open and parse NACA polar data
 with open("data/xf-naca2412-il-100000-n5.csv") as polar_csv:
     polar_reader = csv.DictReader(polar_csv, delimiter=",");
     alpha=[];cl=[];cd=[];cdp=[];cm=[];
@@ -23,6 +47,9 @@ with open("data/xf-naca2412-il-100000-n5.csv") as polar_csv:
 aerofoil1 = Aerofoil("naca_2412",alpha,cl,cd,cdp,cm);
 
 #print(aerofoil1.name)
-print(aerofoil1.alpha)
-#print(aerofoil1.cl)
-#print(aerofoil1.cd)
+#print(aerofoil1.alpha_polar)
+#print(aerofoil1.cl_polar)
+#print(aerofoil1.cd_polar)
+
+cl = aerofoil1.cl(11.001)
+print(cl)
