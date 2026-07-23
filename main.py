0a39dc5e66268ce977c9f7eba9f672136e35a499
JoeTheGitGuy <268192138+JoeTheGitGuy@users.noreply.github.com>
2026-07-24 00:30:51 +0100

    polar aerofoil varname changed in main

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from src.polar import Polar
 def main():
 
     naca_file="data/xf-naca2412-il-100000-n5.csv"
-    polar = load_polar("naca_2412",naca_file)
+    naca2412 = load_polar("naca_2412",naca_file)
 
-    cl = polar.coeff_calc('cl',11.001)
+    cl = naca2412.coeff_calc('cl',11.001)
     print(cl)
 
     return 0
