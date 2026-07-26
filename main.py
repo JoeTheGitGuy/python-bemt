d1e051052e9ee491aeb45e3bab7d925dc3949f80
JoeTheGitGuy <268192138+JoeTheGitGuy@users.noreply.github.com>
2026-07-26 01:04:44 +0100

    Added some basic testing to main

--- main.py
+++ main.py
@@ -8,9 +8,27 @@ def main():
                    load_polar("data/xf-naca2412-il-500000-n5.csv")]
 
     aero1 = Aerofoil("2412_il",naca2412_il)
+
+    if aero1.get_polar(500000).reynolds == 500000:
+        print("Polar for Re=500000 fetches correctly!")
+    if aero1.get_polar(100000).reynolds == 100000:
+            print("Polar for Re=100000 fetches correctly!")
+    if aero1.get_polar(50000).reynolds == 50000:
+                print("Polar for Re=100000 fetches correctly!")
+
     print(aero1.name,":")
+
+    # test suite
+    test_tolerance = 0.001
     polar = aero1.get_polar(500000)
-    print(polar.coeff_calc('cl',11.001))
+    if polar.coeff_calc('cl',11) - 1.3005 <= test_tolerance:
+        print("Polar correctly looked up known datapoint!")
+    else:
+        raise ValueError("Polar FAILED to looked up known datapoint!")
+    if polar.coeff_calc('cl',0.5*(11.000+11.250)) - 0.5*(1.3005+1.3156) <= test_tolerance:
+        print("Polar correctly interpolated between datapoints!")
+    else:
+        raise ValueError("Polar FAILED to interpolate datapoints!")
     return 0
 
 # if someone wants to import this for some reason, do not run on import.
